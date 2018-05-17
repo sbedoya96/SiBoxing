7771b6fd190df3e03774604db3637b11ce9eec62
sergio bedoya <zeu_sergio@hotmail.com>
2018-05-17 04:48:04 -0500

    definicion de loginviewmodel

---
+++ Si_Boxing/Si_Boxing/ViewModels/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Si_Boxing.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand LoginCommand { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+
+        }
+        #endregion
+    }
+
+}
--- Si_Boxing/Si_Boxing/ViewModels/MainViewModel.cs
+++ Si_Boxing/Si_Boxing/ViewModels/MainViewModel.cs
@@ -6,5 +6,24 @@ namespace Si_Boxing.ViewModels
 {
     class MainViewModel
     {
+        
+        #region ViewModels
+
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+
+        #endregion
+        #region Constructors
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
+
